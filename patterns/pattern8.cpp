17af66888e9e0d3c07f2bdd4e7054577ba5f47e8
srishti200426 <srishtimukhija.26@gmail.com>
2025-08-22 10:42:31 +0530

    Added pattern8.cpp

---
+++ patterns/pattern8.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        
+        //space
+        for(int j=0; j < i; j++){
+            cout<<" ";
+        }
+
+        //star
+        for(int j=1;j<= 2*n-(2*i+1); j++){
+            cout<<"*";
+        } 
+
+        //space
+        for(int j=1; j< i; j++){
+            cout<<" ";
+        }
+        cout<<endl;
+        
+    }
+}
