3bf2e65871e33ff817e777204e77584d0e2927d4
srishti200426 <srishtimukhija.26@gmail.com>
2025-08-24 00:54:06 +0530

    Added pattern15.cpp

---
+++ patterns/pattern15.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=n;i>=0;i--){
+        for(char ch='A';ch<='A'+i;ch++){
+            cout<<ch;
+        }
+        cout<<endl;
+    }
+}
