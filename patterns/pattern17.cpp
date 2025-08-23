5126e2ff75e15927383a11751e610cb127218e6d
srishti200426 <srishtimukhija.26@gmail.com>
2025-08-24 00:55:10 +0530

    Added pattern17.cpp

---
+++ patterns/pattern17.cpp
@@ -0,0 +1,40 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin>>N;
+    
+      // Outer loop for the number of rows.
+      for(int i=0;i<N;i++){
+          
+          // for printing the spaces.
+          for(int j=0;j<N-i-1;j++){
+              cout<<" ";
+          }
+          
+          // for printing the characters.
+          char ch = 'A';
+          int breakpoint = (2*i+1)/2;
+          for(int j=1;j<=2*i+1;j++){
+              
+              cout<<ch;
+              if(j <= breakpoint) ch++;
+              else ch--;
+          }
+          
+          // for printing the spaces again after characters.
+          for(int j=0;j<N-i-1;j++){
+              cout<<" ";
+          }
+          
+          // As soon as the letters for each iteration are printed, we move to the
+          // next row and give a line break otherwise all letters
+          // would get printed in 1 line.
+          cout<<endl;
+          
+      }
+}
+
