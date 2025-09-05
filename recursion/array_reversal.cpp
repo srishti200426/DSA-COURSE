16707ab3e09dd99a3ea44178b41514908b0d70db
srishti200426 <srishtimukhija.26@gmail.com>
2025-09-05 23:54:01 +0530

    Added array_reversal.cpp

---
+++ recursion/array_reversal.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+void f(int i ,int arr[],int n){
+    if(i>=n/2){return;}
+
+    else{
+        int temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
+        f(i+1,arr,n);
+    }
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+
+    int arr[n];
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+
+    }
+    f(0,arr,n);
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+
+    }
+
+
+
+}
