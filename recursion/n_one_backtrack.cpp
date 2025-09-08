31e1284694fb117f717fbee70ed63c15c5e76ca0
srishti200426 <srishtimukhija.26@gmail.com>
2025-09-08 23:29:52 +0530

    Added n_one_backtrack.cpp

---
+++ recursion/n_one_backtrack.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void f(int i,int n){
+    if(i>n){return;}
+    else{
+        f(i+1,n);
+        cout<<i<<endl;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    f(1,n);
+}
