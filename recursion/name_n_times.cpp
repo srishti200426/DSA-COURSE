ad0f93493e58d0d372d90b9a8200b5aff234aaa6
srishti200426 <srishtimukhija.26@gmail.com>
2025-09-09 23:51:48 +0530

    Added name_n_times.cpp

---
+++ recursion/name_n_times.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void f(int i,int n){
+    if(i>n){
+        return;
+    }
+
+    else{
+        cout<<"srishti"<<endl;
+        f(i+1,n);
+    }
+
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    f(1,n);
+}
