e0f3d877dfd01e2a3d4a116bc7c9eae2c1f2f89c
srishti200426 <srishtimukhija.26@gmail.com>
2025-09-09 23:51:13 +0530

    Added n_to_one.cpp

---
+++ recursion/n_to_one.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void f(int i,int n){
+    if(i<=0){
+        return;
+    }
+
+    else{
+        cout<<i<<endl;
+        f(i-1,n);
+    }
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+    f(n,n);
+}
