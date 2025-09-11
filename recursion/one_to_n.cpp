6450775098c3427d9a855f0dbd537cedcd31ca75
srishti200426 <srishtimukhija.26@gmail.com>
2025-09-11 21:50:17 +0530

    Added one_to_n.cpp

---
+++ recursion/one_to_n.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void f(int i,int n){
+    if(i>n){
+        return;
+    }
+
+    else{
+        cout<<i<<endl;
+        f(i+1,n);
+
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    f(1,n);
+}
+
+
+
